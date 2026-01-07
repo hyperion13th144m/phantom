3b513194d05c3444125e3ce60ef9b1a85ca11729
hyperion13th144m <hyperion13th144m@gmail.com>
2026-01-08 01:26:37 +0900

    remove unused files

--- mona/src/mona/logger.py
+++ mona/src/mona/logger.py
@@ -1,6 +1,4 @@
 import logging
-from logging import DEBUG, INFO, StreamHandler, getLogger
-from typing import Literal
 
 
 def setup_logger():
@@ -8,17 +6,3 @@ def setup_logger():
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
     )
-
-
-def build_logger(level: Literal["info", "debug"] = "info"):
-    if level == "info":
-        lv = INFO
-    else:
-        lv = DEBUG
-    logger = getLogger(__name__)
-    handler = StreamHandler()
-    handler.setLevel(lv)
-    logger.setLevel(lv)
-    logger.addHandler(handler)
-    logger.propagate = False
-    return logger
