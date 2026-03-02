95a24cbef4440068dd30e10c5f0205a1d1e5e0ff
hyperion13th144m <hyperion13th144m@gmail.com>
2026-03-02 23:45:03 +0900

    rewrite rejection articles on upload data @panther, closes #42

--- panther/src/panther/patent_doc_editor.py
+++ panther/src/panther/patent_doc_editor.py
@@ -33,6 +33,11 @@ class PatentDocEditor:
         dispatchDate = get_date(self.bib.dispatchDate, self.bib.dispatchTime)
         ### 発送系の jp:file-reference-id は全角になっていることがあるので、半角に変換してからESに入れる。
         fileReferenceId = zenkaku_to_hankaku_all(self.bib.fileReferenceId or "")
+        rejectionReasonArticle = [
+            rewrite_rejection_reason_article(r)
+            for r in self.full_text.rejectionReasonArticle or []
+            if r and r.strip()
+        ]
         return EsPatentDoc(
             docId=self.bib.docId,
             task=self.full_text.task,
@@ -68,7 +73,7 @@ class PatentDocEditor:
             abstract=self.full_text.abstract,
             conclusionPartArticle=self.full_text.conclusionPartArticle,
             draftingBody=self.full_text.draftingBody,
-            rejectionReasonArticle=self.full_text.rejectionReasonArticle,
+            rejectionReasonArticle=rejectionReasonArticle,
             opinionContentsArticle=self.full_text.opinionContentsArticle,
             contentsOfAmendment=self.full_text.contentsOfAmendment,
             priorityClaims=self.full_text.priorityClaims,
@@ -127,6 +132,23 @@ def get_images(images_info: list[ImagesInformation]) -> list[ImageInfo]:
                 )
             )
     return images
+
+
+def rewrite_rejection_reason_article(text: str) -> str:
+    reason_pattern = [
+        {
+            "pattern": r"^第１７条の２第３項$",
+            "replacement": "第１７条の２第３項（新規事項）",
+        },
+        {"pattern": r"^第２９条の２$", "replacement": "第２９条の２（拡大先願）"},
+        {"pattern": r"^第２９条第２項$", "replacement": "第２９条第２項（進歩性）"},
+        {"pattern": r"^第３７条$", "replacement": "第３７条（単一性）"},
+    ]
+    for reason in reason_pattern:
+        if re.match(reason["pattern"], text):
+            return reason["replacement"]
+    else:
+        return text
 
 
 if __name__ == "__main__":
